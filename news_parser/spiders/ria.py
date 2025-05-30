e948da828f1fa75ed6183df2eebf2b6fa9b57948
dmsukhikh <dimasssuchich@mail.ru>
2025-05-30 20:49:17 +0300

    feat: basic url fetching logic

--- news_parser/spiders/ria.py
+++ news_parser/spiders/ria.py
@@ -1,10 +1,34 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 
-class RiaSpider(scrapy.Spider):
+class RiaSpider(CrawlSpider):
     name = "ria"
     allowed_domains = ["ria.ru"]
-    start_urls = ["https://ria.ru"]
+    # start_urls = ["https://ria.ru"] 
+    custom_settings = {
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0",
+        "DEPTH_LIMIT": 10,
+        "DOWNLOAD_DELAY": 1.5,
+        "ROBOTSTXT_OBEY": True
+    }
+
+    rules = (
+        Rule(link_extractor=LinkExtractor(
+            allow_domains="ria.ru", allow="\\.html$")),
+    )
 
     def parse(self, response):
-        pass
+        parsed_article_text = ""
+
+        for block in response.css("div[class=article__block]"):
+            if ("data-type" not in block.attrib):
+                continue
+
+            if (block.attrib["data-type"] == "text"):
+                for part in block.css("div[class=article__text]"):
+                    parsed_article_text += part.css("*::text").get()
+
+        yield {"content": parsed_article_text}
+
