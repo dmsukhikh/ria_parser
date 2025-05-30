32103c481e69bd528b4ee258f8f845b5ba011c77
dmsukhikh <dimasssuchich@mail.ru>
2025-05-31 00:02:53 +0300

    fix: little fixes

--- crawler/spiders/ria.py
+++ crawler/spiders/ria.py
@@ -7,18 +7,18 @@ import texttable
 class RiaSpider(CrawlSpider):
     name = "ria"
     allowed_domains = ["ria.ru"]
-    start_urls = ["https://ria.ru/20250530/tribunal-2019960722.html"]
+    start_urls = ["https://ria.ru/20250530/putin-2020082829.html"]
     # start_urls = ["https://ria.ru"] 
     custom_settings = {
         "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0",
-        "DEPTH_LIMIT": 5,
+        "DEPTH_LIMIT": 10,
         "DOWNLOAD_DELAY": 1.5,
         "ROBOTSTXT_OBEY": True
     }
 
     rules = (
         Rule(link_extractor=LinkExtractor(
-            allow_domains="ria.ru", allow=r"\/\d+\/.+\.html"), callback="parse"),
+            allow_domains="ria.ru", allow=r"\/\d+\/.+html"), callback="parse"),
     )
 
     def _get_plain_text(self, html_text : str) -> str:
