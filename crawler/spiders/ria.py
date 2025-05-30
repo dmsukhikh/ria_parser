27ba11d32d1e6e8dd24ae642a2eaef2c4a0c046a
dmsukhikh <dimasssuchich@mail.ru>
2025-05-30 23:53:08 +0300

    feat: impl full output from crawler

--- crawler/spiders/ria.py
+++ crawler/spiders/ria.py
@@ -1,24 +1,24 @@
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 import re
-import typing
 import texttable
 
 
 class RiaSpider(CrawlSpider):
     name = "ria"
     allowed_domains = ["ria.ru"]
+    start_urls = ["https://ria.ru/20250530/tribunal-2019960722.html"]
     # start_urls = ["https://ria.ru"] 
     custom_settings = {
         "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0",
-        "DEPTH_LIMIT": 10,
+        "DEPTH_LIMIT": 5,
         "DOWNLOAD_DELAY": 1.5,
         "ROBOTSTXT_OBEY": True
     }
 
     rules = (
         Rule(link_extractor=LinkExtractor(
-            allow_domains="ria.ru", allow="\\.html$")),
+            allow_domains="ria.ru", allow=r"\/\d+\/.+\.html"), callback="parse"),
     )
 
     def _get_plain_text(self, html_text : str) -> str:
@@ -26,6 +26,11 @@ class RiaSpider(CrawlSpider):
 
     def parse(self, response):
         parsed_article_text = ""
+
+        date = response.css(
+            "div[class=article__info-date]").css("a::text").get()
+        
+        header = response.css("div[class=article__title]::text").get()
 
         for block in response.css("div[class=article__block]"):
             if "data-type" not in block.attrib:
@@ -52,11 +57,12 @@ class RiaSpider(CrawlSpider):
                 parsed_article_text += "\n" + \
                     block.css(
                         block.attrib["data-type"]+"::text").get() + "\n"
-
             else:
                 continue
 
             parsed_article_text += "\n"
 
-        yield {"content": parsed_article_text}
-
+        yield {"date": date,
+               "header": header,
+               "url": re.sub("\\?.*$", "", response.url),
+               "content": parsed_article_text}
